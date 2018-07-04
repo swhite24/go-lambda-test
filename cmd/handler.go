b6d2b9ff42b0e5911f1f7c7ffb333064e9213183
Steven White <swhitewvu24@gmail.com>
2018-07-04 14:09:47 -0400

    allow local execution

--- cmd/handler.go
+++ cmd/handler.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/apex/gateway"
 	"github.com/gin-gonic/gin"
 	"github.com/swhite24/go-lambda-test/pkg/controllers"
 )
 
 func main() {
+	addr := ":3000"
+	mode := os.Getenv("GIN_MODE")
 	g := gin.New()
 
 	controllers.ServeUsers(g, "golang-test")
 
-	gateway.ListenAndServe(":3000", g)
+	if mode == "release" {
+		log.Fatal(gateway.ListenAndServe(addr, g))
+	} else {
+		log.Fatal(http.ListenAndServe(addr, g))
+	}
 }
